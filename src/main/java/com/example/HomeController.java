ac04eeb2ec845f52eed314b08eda6d7c322ef59e
markFT39 <43339545+markFT39@users.noreply.github.com>
2022-12-09 09:45:09 +0900

    login interceptor bug fix

--- src/main/java/com/example/HomeController.java
+++ src/main/java/com/example/HomeController.java
@@ -3,10 +3,15 @@ package com.example;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String home() {
-        return "login/login";
+    public String home(HttpSession session) {
+        if (session.getAttribute("login") != null ){
+            session.removeAttribute("login");
+        }
+        return "redirect:/login/login";
     }
 }
